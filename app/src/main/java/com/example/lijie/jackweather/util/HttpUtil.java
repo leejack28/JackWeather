296c73458294beb425b363ac8af59bf9f047ef9f
leejack28 <upfighting@sina.com>
2017-02-08 21:08:45 +0800

    完成遍历省市县三级列表的功能

---
+++ app/src/main/java/com/example/lijie/jackweather/util/HttpUtil.java
@@ -0,0 +1,17 @@
+package com.example.lijie.jackweather.util;
+
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+/**
+ * Created by Administrator on 2017-02-08.
+ */
+
+public class HttpUtil {
+    public static void sendOkHttpRequest(String address, Callback callback){
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(address).build();
+        client.newCall(request).enqueue(callback);
+    }
+}
